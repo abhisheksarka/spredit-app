53f39d83ec5aed25d159a5afee732112dfb4cc8d
Abhishek Sarkar <abhisheksarka@gmail.com>
2014-11-22 19:43:54 +0530

    icon added to landing page

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -10,9 +10,12 @@ angular.module('ms').run(['$templateCache', function($templateCache) {
     "<div id=\"bg-backdrop\"></div>\n" +
     "<div class=\"row\">\n" +
     "  <div class=\"col-md-12 text-center\">\n" +
+    "    <div class=\"logo logo-128\">\n" +
+    "      <img src=\"../images/logo_128.png\"/>\n" +
+    "    </div>\n" +
     "    <h1 class=\"brand-name\">Maskd</h1>\n" +
     "    <h2 class=\"brand-tagline\">When the world around you goes anonymous</h2>\n" +
-    "    <button class=\"btn btn-success fb-sign-in\">Sign in with facebook</button>\n" +
+    "    <button class=\"btn btn-warning fb-sign-in\">Sign in with facebook</button>\n" +
     "  </div>\n" +
     "</div>\n"
   );
