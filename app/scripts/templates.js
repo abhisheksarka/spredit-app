9cd1ab64748e19a2ccef9b0ead9ed13ef1770e66
Abhishek Sarkar <abhisheksarka@gmail.com>
2014-12-21 03:29:41 +0530

    new direction, new landing page

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -3,7 +3,7 @@ angular.module('ms').run(['$templateCache', function($templateCache) {
 
   $templateCache.put('app/scripts/components/fb_sdk/log_in/template.html',
     "<span>\n" +
-    "  <button class=\"btn btn-warning fb-sign-in\" data-ng-click=\"login()\">Sign in with facebook</button>\n" +
+    "  <button class=\"btn btn-success fb-sign-in\" data-ng-click=\"login()\">Sign in with facebook</button>\n" +
     "</span>"
   );
 
@@ -12,7 +12,7 @@ angular.module('ms').run(['$templateCache', function($templateCache) {
     "<nav class=\"navbar navbar-default\" role=\"navigation\" data-ng-if=\"display\">\n" +
     "  <div class=\"container\">\n" +
     "    <div class=\"navbar-header\">\n" +
-    "      <a class=\"navbar-brand\" href=\"/#home\">\n" +
+    "      <a class=\"navbar-brand\" href=\"#home\">\n" +
     "        <span class=\"logo-tiny\">\n" +
     "          <img src=\"images/logo_white_40.png\"/>\n" +
     "        </span>\n" +
@@ -36,9 +36,9 @@ angular.module('ms').run(['$templateCache', function($templateCache) {
     "    <div class=\"logo logo-128\">\n" +
     "      <img src=\"../images/logo_128.png\"/>\n" +
     "    </div>\n" +
-    "    <h1 class=\"brand-name\">Maskd</h1>\n" +
+    "    <h1 class=\"brand-name\">Strikr</h1>\n" +
     "    <h2 class=\"brand-tagline\">\n" +
-    "      Share with friends, ask questions or tag them, <br>anonymously...\n" +
+    "      Listen to conversations, <br> Participate or share them with your friends and the world \n" +
     "    </h2>\n" +
     "    <ms-fb-log-in \n" +
     "      data-connected=\"connected(response)\" \n" +
@@ -46,8 +46,7 @@ angular.module('ms').run(['$templateCache', function($templateCache) {
     "      data-unknown=\"unknown(response)\"></ms-fb-log-in>\n" +
     "\n" +
     "    <p class=\"quote\">\n" +
-    "      <i>Man is least himself when talks in his own person. Give him a mask, and he will tell you the truth <br>- Oscar Wilde</i>\n" +
-    "    </p>  \n" +
+    "          </p>  \n" +
     "  </div>\n" +
     "</div>\n"
   );
