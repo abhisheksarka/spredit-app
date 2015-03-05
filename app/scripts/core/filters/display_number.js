13da5fcad430822ed789297ceec8c14de7e2260b
Abhishek Sarkar <abhisheksarka@gmail.com>
2015-03-05 19:01:34 +0530

    display number directive fixed

--- app/scripts/core/filters/display_number.js
+++ app/scripts/core/filters/display_number.js
@@ -5,11 +5,22 @@
     };
 
     function format(num) {
-      n = parseInt(num);
-      if(n <= 9 && n > 0) {
-        return "0" + num;
+      n = parseInt(num),
+      nv = n;
+      
+      if (n < 999) {
+        return nv;
+      } else if (n >= 999 && n < 999999) {
+        nv = (n/1000).toFixed(1) + 'K';
+      } else if (n >= 999999 && n < 999999999) {
+        nv = (n/1000000).toFixed(1) + 'M';
       };
-      return num;
+
+      if(nv) {
+        nv = nv.toString().replace('.0', ''); 
+      };
+
+      return nv;
     };
   };
   angular.module('ms.core.filters')
@@ -17,3 +28,18 @@
     Filter
   ]);
 }());
+
+// num <= 999
+// num
+// 1,000 - 999,999
+// 1k - 999K
+
+// 1,000,000 - 999,999,999
+// 1M - 999M
+
+// 1,000,000,000 - 999,999,999,999
+// 1B - 999B
+
+
+
+
